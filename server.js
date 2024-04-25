c55f3f0afaab31918e24fccef5ef73084d894280
BED_NOOB <bednoobcontact@gmail.com>
2024-04-25 16:51:40 -0400

    image gap test

--- server.js
+++ server.js
@@ -425,7 +425,7 @@ app.get("/account/:user", async (req, res) => {
             .shape {
                 width: 30px;
                 float: left:
-                gap: 10px;
+                gap: 100px;
             }
 
             .search-input {
