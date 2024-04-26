0cd6186ae56464af9769cbe6c141c808422048ed
BED_NOOB <bednoobcontact@gmail.com>
2024-04-26 08:07:04 -0400

    text test

--- server.js
+++ server.js
@@ -630,6 +630,7 @@ app.get("/account/:user", async (req, res) => {
                 <div class="box-value">
                     <div class="box-title">Polygon Gallery</div>
                     <hr class="hr2">
+                    <p>Radiant: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9<p>
                     <img class="shape" src= ${polygonGalleryImage5}
                     <div>${(reversedArray[5])}</div>
                     <img class="shape" src= ${polygonGalleryImage6}
