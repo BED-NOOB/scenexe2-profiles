7848bca4d70cb166a22665ff09575b97de5ccee1
BED_NOOB <bednoobcontact@gmail.com>
2024-04-26 17:01:31 -0400

    aa

--- server.js
+++ server.js
@@ -544,7 +544,7 @@ app.get("/account/:user", async (req, res) => {
                 border: solid 2px black;
                 border-radius: 5px;
                 padding: 20px;
-                margin-top: 25px;
+                margin-top: 0px;
                 font-size: 25px;
                 z-index: 100;
             }
